7fd84d1c6c9c12f8b8782043b2135ffb9a8d9ece
Ahmed-Mashali <ahmed.mashali@vultara.com>
2025-07-17 19:11:12 +0300

    Update app.module with configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { mongooseConfig } from './config/database.config';
+import { UserModule } from './user/user.module';
+import { validationSchema } from './config/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema,
+    }),
+    MongooseModule.forRootAsync(mongooseConfig),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
